5c48dbab1a2d2a994bba1260b91a6a241d1d990e
patrickwhardy <patrickwhardy@gmail.com>
2016-02-23 21:42:10 -0700

    everything working but write html

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,20 +1,45 @@
 require 'kramdown'
 require 'Find'
+require 'pry'
 
 class Converter
+  attr_reader :html_text
   def initialize(parent_dir)
     @parent_dir = parent_dir
   end
 
-  def find_markdown
-    Dir.glob("#{@parent_dir}/**/*.markdown")
-    #Dir.foreach(@parent_dir)
+  def create_output
+    find_markdown
+    convert_md
+    build_output_folders
+    write_html_files
+  end
 
-    # * == anyfile/dir name
-  #   md_file_paths = []
-  #   Find.find(@parent_dir) do |path|
-  #     md_file_paths << path if path =~ /.*\.md$/
-  #   end
-  #   md_file_paths
+#PASSING IN MACHINE-SPECIFIC PARENT DIR
+#NEED TO USE ~/ WITHOUT ERROR!!!
+  def find_markdown
+    @md_files = Dir.glob("#{@parent_dir}/**/*.markdown")
    end
+
+   def convert_md
+     @html_text = @md_files.map do |file|
+      Kramdown::Document.new(File.read(file)).to_html
+     end
+     @html_text
+   end
+
+   def build_output_folders
+     @output_dir = "#{@parent_dir}/_output"
+     Dir.mkdir("#{@output_dir}/css")
+       File.write("#{@output_dir}/css/main.css", File.read("#{@parent_dir}/source/css/main.css"))
+     Dir.mkdir("#{@output_dir}/pages")
+     Dir.mkdir("#{@output_dir}/posts")
+   end
+
+   def write_html_files
+     File.write("#{@output_dir}/index.html", @html_text[0])
+     File.write("#{@output_dir}/pages/about.html", @html_text[1])
+     File.write("#{@output_dir}/posts/#{date}-welcome-to-hyde.html", @html_text[2])
+   end
+
 end
--- test/converter_test.rb
+++ test/converter_test.rb
@@ -5,23 +5,37 @@ require_relative '../lib/converter'
 
 class ConverterTest < Minitest::Test
   def setup
-    @converter = Converter.new("/Users/patrickwhardy/my_sweet_blog")
+    @parent_directory = "/Users/patrickwhardy/my_sweet_blog"
+    @converter = Converter.new(@parent_directory)
+    @date = Date.today.strftime("%Y-%m-%d")
   end
 
   def test_find_markdown_returns_markdown_files
-    date = Date.today.strftime("%Y-%m-%d")
-    assert_equal ["about.markdown", "index.markdown", "#{date}-welcome-to-hyde.markdown"], @converter.find_markdown
+    assert_equal [ "#{@parent_directory}/source/index.markdown", "#{@parent_directory}/source/pages/about.markdown", "#{@parent_directory}/source/posts/#{@date}-welcome-to-hyde.markdown"], @converter.find_markdown
+  end
+#this needs the following to be in all .md files
+# "# Some Markdown\n\n* a list\n* another item"
+  def test_convert_md_intakes_markdown_and_outputs_html
+    @converter.find_markdown
+    @converter.convert_md
+    assert_equal ["<p>”# Some Markdown\\n\\n* a list\\n* another item”</p>\n",
+ "<p>”# Some Markdown\\n\\n* a list\\n* another item”</p>\n",
+ "<p>”# Some Markdown\\n\\n* a list\\n* another item”</p>\n"], @converter.html_text
   end
 
-  def test_convert_intakes_markdown_and_outputs_html
-
+  def test_build_output_folders_makes_dirs_and_css
+    @converter.build_output_folders
+    Dir.exists?("~/my_sweet_blog/_output/css")
+    Dir.exists?("~/my_sweet_blog/_output/pages")
+    Dir.exists?("~/my_sweet_blog/_output/posts")
+    File.exists?("~/my_sweet_blog/_output/css/main.css")
   end
 
-  def test_METHOD_copies_non_markdown_files_into_output
-
+  def test_write_html_files_copies_directories_in_output
+    @converter.create_output
+    File.exists?("~/my_sweet_blog/_output/index.html")
+    File.exists?("~/my_sweet_blog/_output/pages/about.html")
+    File.exists?("~/my_sweet_blog/_output/posts/#{date}-welcome-to-hyde.html")
   end
-
-  def test_METHOD_copies_directories_in_output
-
-  end
+  #Errno::EEXIST: File exists @ dir_s_mkdir - /Users/patrickwhardy/my_sweet_blog/_output/css
 end
