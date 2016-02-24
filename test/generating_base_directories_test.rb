54c83b5abf8c92e97ff44fe43658977b96e0d3c8
Jennifer <jennifersoden@hotmail.com>
2016-02-24 14:49:58 -0700

    gah

--- test/generating_base_directories_test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require_relative 'generating_base_directories'
-
-class GeneratingBaseDirectoriesTest
-  gbd = GeneratingBaseDirectories.new
-  gbd.make_directory(poopbutts)
-
-end
