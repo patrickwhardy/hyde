1b938ee3418802261381d577ee4b9d304201aa53
Jennifer <jennifersoden@hotmail.com>
2016-02-25 22:05:12 -0700

    testing

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -3,6 +3,7 @@ require 'Date'
 require "minitest/autorun"
 require 'minitest/pride'
 require_relative "../lib/structure"
+require_relative "../lib/new_blog_files"
 require_relative '../lib/converter'
 
 class ConverterTest < Minitest::Test
@@ -18,13 +19,15 @@ class ConverterTest < Minitest::Test
 
   def test_find_markdown_returns_markdown_files
     assert_equal ["#{@parent_directory}/source/index.markdown",
-       "#{@parent_directory}/source/pages/about.markdown",
-        "#{@parent_directory}/source/posts/#{@converter.date}-welcome-to-hyde.markdown"], @converter.md_files
+    "#{@parent_directory}/source/pages/about.markdown",
+    "#{@parent_directory}/source/posts/#{@converter.date}-welcome-to-hyde.markdown"], @converter.md_files
   end
 
   def test_convert_md_intakes_markdown_and_outputs_html
-#when you make a markdown, it takes markdown from source and
-#makes an html file in output
+    
+    assert File.exists?("#{@parent_directory}/_output/index.html")
+    assert File.exists?("#{@parent_directory}/_output/pages/about.html")
+    assert File.exists?("#{@parent_directory}/_output/posts/#{@converter.date}-welcome-to-hyde.html")
   end
 
   def test_build_output_folders_makes_dirs_and_css
@@ -32,12 +35,6 @@ class ConverterTest < Minitest::Test
     assert Dir.exists?("#{@parent_directory}/_output/pages")
     assert Dir.exists?("#{@parent_directory}/_output/posts")
     assert File.exists?("#{@parent_directory}/_output/css/main.css")
-  end
-
-  def test_write_html_files_copies_directories_in_output
-    assert File.exists?("#{@parent_directory}/_output/index.html")
-    assert File.exists?("#{@parent_directory}/_output/pages/about.html")
-    assert File.exists?("#{@parent_directory}/_output/posts/#{@converter.date}-welcome-to-hyde.html")
   end
 
 end
--- test/structure_test.rb
+++ test/structure_test.rb
@@ -1,6 +1,6 @@
 require_relative 'test_helper'
 require "minitest/autorun"
-require "../lib/structure"
+require_relative "../lib/structure"
 
 class StructureTest < Minitest::Test
   def setup
@@ -8,26 +8,27 @@ class StructureTest < Minitest::Test
     @structure = Structure.new(@parent_directory)
   end
 
+  def teardown
+    FileUtils.rm_rf(@parent_directory)
+  end
+
   def test_parent_dir_can_be_created
     assert Dir.exists?(@parent_directory)
   end
 
-  # def test_raises_argument_error_if_dir_exists
-  #   @structure = Structure.new(@parent_directory)
-  #   raise ArgumentError, Dir.exists?(@parent_directory)
-  # end
+  def test_raises_argument_error_if_dir_exists
+    assert_raises(ArgumentError) { @structure = Structure.new(@parent_directory) }
+  end
 
-  # def test_can_build_a_folder_structure
-  #   check for existance of
-  #     parent_dir/_output
-  #
-  # end
+  def test_can_build_a_folder_within_the_structure
+    assert Dir.exists?("#{@parent_directory}/source/layouts")
+    assert Dir.exists?("#{@parent_directory}/source/css")
+    assert Dir.exists?("#{@parent_directory}/source/posts")
+  end
 
-  #def test_can_create_base_files
-    #check exsistance of date-welcometomarkdown
-
-  def teardown
-    FileUtils.rm_rf(@parent_directory)
+  def test_can_create_files_within_the_structure
+    assert File.exists?("#{@parent_directory}/source/layouts/default.html.erb")
+      assert File.exists?("#{@parent_directory}/source/pages/about.markdown")
   end
 
 end
